e9a154bca82c9be5b4094776f81fa4729253174f
pkthunder87 <pkthunder87@gmail.com>
2023-08-28 17:02:27 -0700

    Lesson 195: Handling New Answers

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,12 +1,12 @@
 import Options from "./Options";
 
-function Question({ question }) {
+function Question({ question, dispatch, answer }) {
   console.log(question);
   return (
     <div>
       <h4>{question.question}</h4>
       <div className="options">
-        <Options question={question} />
+        <Options question={question} dispatch={dispatch} answer={answer} />
       </div>
     </div>
   );
